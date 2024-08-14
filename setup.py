805839a4abfa15cb4f3f6f1b16b1960f0cd9ce38
Roy Ernst <rernstdev@gmail.com>
2024-08-14 16:59:04 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import subprocess
 import sys
 
 def run_script(script_name):
-    """Run the specified script."""
+    """Run the specified setup script."""
     try:
         subprocess.check_call([script_name])
     except subprocess.CalledProcessError as e:
@@ -11,12 +11,17 @@ def run_script(script_name):
         sys.exit(1)
 
 def main():
+    if not os.path.exists('requirements.txt'):
+        # Exit if no requirements.txt file found -- it is needed for both setup scripts
+        print("No requirements.txt file found. Please make sure it exists in the current directory.")
+        return
+        
     if os.name == 'nt':
         # Windows
-        script_name = 'setup_env.bat'
+        script_name = 'setup.bat'
     else:
         # macOS/Linux
-        script_name = 'setup_env.sh'
+        script_name = 'setup.sh'
     
     if not os.path.exists(script_name):
         print(f"{script_name} not found. Please make sure it exists in the current directory.")
